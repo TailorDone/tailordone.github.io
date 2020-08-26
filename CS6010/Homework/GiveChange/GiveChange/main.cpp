fc65e9ac5af5417db99c99fc838fc3700c6c5ab9
Taylor <taylorjodunn@gmail.com>
2020-08-26 09:29:55 -0600

    Added solution using mod. Added comments for clarity.

--- CS6010/Homework/GiveChange/GiveChange/main.cpp
+++ CS6010/Homework/GiveChange/GiveChange/main.cpp
@@ -15,16 +15,31 @@ int main(int argc, const char * argv[]) {
     int amountGiven;
     std::cout <<"Enter the amount paid in cents?\n";
     std::cin >> amountGiven;
+    //Coins that can be given by the machine
     int quarters, dimes, nickels, pennies;
     int totalChange = amountGiven - costOfItem;
+    //Determine the number of quarters to give back
     quarters = totalChange / 25;
     int remainderChange = totalChange;
-    remainderChange = remainderChange - quarters * 25;
+    //Change to be given after quarters have been determined
+    remainderChange = totalChange % 25;
+    //Determine the number of dimes to give back
     dimes = remainderChange / 10;
-    remainderChange = remainderChange - dimes * 10;
+    //Change to be given after dimes have been determined
+    remainderChange = remainderChange % 10;
+    //Determine the number of nickles
     nickels = remainderChange / 5;
-    remainderChange = remainderChange - nickels * 5;
-    pennies = remainderChange;
+    //Determine the number of pennies
+    pennies = remainderChange % 5;
     std::cout << "Change: " << totalChange << " \nQuarters: " << quarters << "\nDimes: " << dimes << "\nNickles: " << nickels << "\nPennies: " << pennies << std::endl;
     return 0;
 }
+
+//    Alternative Solution Not Using Mod
+//    Replace line 22-26 with the following
+//    remainderChange = remainderChange - quarters * 25;
+//    dimes = remainderChange / 10;
+//    remainderChange = remainderChange - dimes * 10;
+//    nickels = remainderChange / 5;
+//    remainderChange = remainderChange - nickels * 5;
+//    pennies = remainderChange;
